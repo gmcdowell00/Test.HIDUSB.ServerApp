1e1ce5ef5125a8acf038eaa4fa543ddacc37db98
Glen McDowell <gmcdowell00@gmail.com>
2022-12-24 13:14:23 -0500

    Add project files.

---
+++ Test.HIDUSB.ServerApp/Pages/TestHIDComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Test.HIDUSB.ServerApp.Pages
+{
+    // Input Enum
+    // Represnet foot pedal input and input combinations
+    // 6 permutations and 1 combinations 
+    enum FootPedal { KEYUP, REW, PLAY, REWPLAY, FWD, REWFWD, FWDPLAY }
+
+    public class TestHIDComponent : ComponentBase
+    {
+        /// <summary>
+        /// Invokable JavaScript Method
+        /// Prints foot pedal input
+        /// </summary>
+        /// <param name="i"></param>
+        [JSInvokable]
+        public static void ReceivedInput(int i)
+        {
+            // Print input
+            System.Diagnostics.Debug.Print($"Foot Pedal Input: {(FootPedal)i}");
+        }
+    }
+}
---
+++ Test.HIDUSB.ServerApp/wwwroot/js/hid.js
@@ -0,0 +1,103 @@
+﻿// Maps foot pedal inputs to value
+const map = new Map();
+map.set(0, "KEYUP");
+map.set(1, "REW");
+map.set(2, "PLAY");
+map.set(3, "REWPLAY");
+map.set(4, "FWD");
+map.set(5, "REWFWD");
+map.set(6, "FWDPLAY");
+
+// Attach EventListeners when TestHIDComponent is rendered
+window.attachHandlers = () => {
+
+    // Get Device Button
+    document.getElementById("devices").addEventListener("click", getDevices);
+
+    // Request Device Button
+    document.getElementById("request").addEventListener("click", requestDevice);
+};
+
+// Log input from HID device
+// I'm capturing the HID event in the client and invoking a server-side method. 
+// Try binding a server-side variable to the client to invoke the server function directly.
+function handleInputReport(e) {
+    console.log(e.device.productName + ": got input report " + e.reportId);
+    console.log(new Uint8Array(e.data.buffer));
+    let input = new Uint8Array(e.data.buffer);
+    if (input[0] != 0) {
+        document.getElementById("input").innerHTML = map.get(input[0]);
+        DotNet.invokeMethodAsync("Test.HIDUSB.ServerApp", "ReceivedInput", input[0]);
+    }        
+}
+
+// Get Devices
+// HID interface gets a list of the connected HID devices that the user has 
+// previously been granted access to in response
+// https://developer.mozilla.org/en-US/docs/Web/API/HID/getDevices
+async function getDevices() {
+    const devices = await navigator.hid.getDevices();
+    var me = 0;
+}
+
+// Request Device
+// HID interface requests access to a HID device.
+// The user agent will present a permission dialog including a list of connected devices, 
+// and ask the user to select and grant permission to one of these devices.
+// https://developer.mozilla.org/en-US/docs/Web/API/HID/requestDevice
+async function requestDevice () {
+
+    // If 'hid' (Web HID API) availible
+    if ("hid" in navigator) {
+
+        // Variable
+        let device;
+
+        // Start try
+        try {
+
+            // Get Requested device matching vendorID and productId
+            const devices = await navigator.hid.requestDevice({
+                filters: [
+                    {
+                        vendorId: 0x05f3,
+                        productId: 0x00ff,
+                    },
+                ],
+            });
+
+            // If devices aren't availible
+            if (devices.length == 0) {
+                alert('No Device Detected');
+                return;
+            }
+
+            // Grab first device
+            device = devices[0];
+
+            // If device is already open
+            if (device.open()) {
+
+                // Attach report
+                device.addEventListener("inputreport", handleInputReport);
+                return;
+            }
+
+            // Open Device and attach report
+            devices.open().then(() => {
+                console.log("Opened device: " + device.productName);
+                device.addEventListener("inputreport", handleInputReport);
+            });
+            
+        } catch (error) {
+            console.log("An error occurred.");
+        }
+
+        // Write log
+        if (!device) {
+            console.log("No device was selected.");
+        } else {
+            console.log(`HID: ${device.productName}`);
+        }
+    }
+}
